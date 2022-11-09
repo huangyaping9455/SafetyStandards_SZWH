5ed582fd358048217f5fe0089b965fe2728e47d3
Dawn <it_xx136@163.com>
2022-11-09 12:58:42 +0800

    【A】2022年11月09日提交

--- blade-service/blade-anbiao/src/main/java/org/springblade/anbiao/weixiucheliang/service/impl/MaintenanceServiceImpl.java
+++ blade-service/blade-anbiao/src/main/java/org/springblade/anbiao/weixiucheliang/service/impl/MaintenanceServiceImpl.java
@@ -17,35 +17,35 @@ import java.util.List;
  * @Date :2022/11/4 14:32
  */
 public class MaintenanceServiceImpl implements MaintenanceService {
-	private MaintenanceMapper partsMapper;
-
-	@Override
-	public List<MaintenanceVO> selectList(MaintenancePage maintenancePage) {
-		int pageSize = maintenancePage.getSize();
-		int pageNum = maintenancePage.getCurrent();
-		PageHelper.startPage(pageNum,pageSize);
-		List<MaintenanceVO> maintenanceVOS = partsMapper.selectList(maintenancePage);
-		PageInfo<MaintenanceVO> pageInfo = new PageInfo(maintenanceVOS);
-		return pageInfo.getList();
-	}
-
-	@Override
-	public List<MaintenanceEntity> selectAll(String rid) {
-		return partsMapper.selectAll(rid);
-	}
-
-	@Override
-	public Boolean insertOne(MaintenanceDTO maintenanceDTO) {
-		return partsMapper.insertOne(maintenanceDTO);
-	}
-
-	@Override
-	public Boolean deleteAccident(String id) {
-		return partsMapper.deleteMain(id);
-	}
-
-	@Override
-	public Boolean updateAccident(MaintenanceDTO maintenanceDTO) {
-		return partsMapper.updateMain(maintenanceDTO);
-	}
+//	private MaintenanceMapper partsMapper;
+//
+//	@Override
+//	public List<MaintenanceVO> selectList(MaintenancePage maintenancePage) {
+//		int pageSize = maintenancePage.getSize();
+//		int pageNum = maintenancePage.getCurrent();
+//		PageHelper.startPage(pageNum,pageSize);
+//		List<MaintenanceVO> maintenanceVOS = partsMapper.selectList(maintenancePage);
+//		PageInfo<MaintenanceVO> pageInfo = new PageInfo(maintenanceVOS);
+//		return pageInfo.getList();
+//	}
+//
+//	@Override
+//	public List<MaintenanceEntity> selectAll(String rid) {
+//		return partsMapper.selectAll(rid);
+//	}
+//
+//	@Override
+//	public Boolean insertOne(MaintenanceDTO maintenanceDTO) {
+//		return partsMapper.insertOne(maintenanceDTO);
+//	}
+//
+//	@Override
+//	public Boolean deleteAccident(String id) {
+//		return partsMapper.deleteMain(id);
+//	}
+//
+//	@Override
+//	public Boolean updateAccident(MaintenanceDTO maintenanceDTO) {
+//		return partsMapper.updateMain(maintenanceDTO);
+//	}
 }
